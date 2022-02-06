0c507a070e1aaa598a596885c3660da1e994d88e
unknown <tpgus1196@naver.com>
2022-02-07 06:33:55 +0900

    #3.7 error:createdAt 출력 안됨

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
 import {ApolloServer, gql} from "apollo-server";
-//import { PrismaClient } from "@prisma/client";
-import pkg from '@prisma/client';
-
 import {ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
-//이 코드 추가해줘야 sandbox로 안넘어가고 진행 가능
+import {PrismaClient} from '@prisma/client';
+// "type": "module"
+//package.json에서 제거함
 
-//클라언트 생성
-const {PrismaClient} = pkg;
+
+// import pkg from '@prisma/client';
+// const {PrismaClient} = pkg;
+
 const client = new PrismaClient(); 
-//@prisma/client에서 import되는 것
-//설명문에서 movies를 활용한 예시 보여줌
-//이 클라이언트는 우리 스키마에 맞춤형으로 생성된 것
 
-//type definition 외 graphQL 파일을 사용할 수도 있다
 const typeDefs = gql`
     type Movie{
         id: Int!
@@ -25,35 +22,38 @@ const typeDefs = gql`
     }
     type Query{
         movies: [Movie]
-        movie: Movie
+        movie(id:Int!): Movie
     }
     type Mutation{
-        createMovie(title:String!):Boolean
-        deleteMovie(title:String!):Boolean
+        createMovie(title:String!, year:Int!, genre: String):Movie
+        deleteMovie(id:Int!):Movie
+        updateMovie(id:Int! year:Int!):Movie
     }
-`;
+`;//mutation에 year, genre title 추가하기
+    //createMovie의 반환 값을 boolean에서 Movie로 수정
+    //create가 반환하는 값이 Movie임
+    //deleteMovie도 마찬가지
 
+    //id로 새로운 영화 찾기(query movie(id:Int!)로 수정)
 
 const resolvers = {
     Query: {
-        movies: () => client.movie.findMany(), //우리 데이터베이스로 가서 모든 영화들 검색
-        movie: () => ({"title": "Hello", year: 2022}),
+        movies: () => client.movie.findMany(), //새로운 client에 대해 하는 작업
+        movie: (_,{id}) => client.movie.findUnique({where:{id}}),
     },
     Mutation: {
-        //괄호 내 root, {title}, context, info 가진다 
-        //root 대신 _ 사용 가능(아무거나 다 가능): _쓰는 이유는 첫 번째 인자 무시하기 위해
-        //두번째인 인자가 mutation이나 query에게 주는 argument
-        //createMovie: (_, {title}) 이렇게 적는 대신 인자들을 열어 원하는 것들 꺼낼 수 있다.
-        //=구조 분해 할당(destructuring)
-        createMovie: (_, {title, year, genre}) => {
-            console.log({title});
-            return true;
-        }, 
-        deleteMovie: (_, {title}) => {
-            console.log({title});
-            return true;
-        },
-    }
+        createMovie: (_, {title, year, genre}) => 
+        client.movie.create({ //title:title 할 필요 없음
+            data: {
+              title,
+              year,
+              genre,
+            },
+        }),
+        deleteMovie: (_, {id}) => client.movie.delete({where:{id}}),
+        updateMovie: (_, {id, year}) => 
+            client.movie.update({where: {id}, data:{year}})
+    },
 };
 
 const server = new ApolloServer({
